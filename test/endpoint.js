791b397abc2c2c5f727fda5665690773a57e713d
Ali Haider SDE <ali.haider@tamba.ca>
2025-07-30 23:47:50 +0500

    test: add GET budget tests

--- test/endpoint.js
+++ test/endpoint.js
@@ -4,6 +4,8 @@ const http = require('http')
 const test = require('tape')
 const servertest = require('servertest')
 const app = require('../lib/app')
+const db = require('../lib/db')
+require('../scripts/seed')
 
 const server = http.createServer(app)
 
@@ -30,4 +32,20 @@ test('GET /nonexistent should return 404', function (t) {
     t.equal(res.statusCode, 404, 'Should return 404')
     t.end()
   })
+})
+
+test('GET /api/project/budget/:id should return 200 and project data', function (t) {
+  servertest(server, '/api/project/budget/1', { encoding: 'json' }, function (err, res) {
+    t.error(err, 'No error')
+    t.equal(res.statusCode, 200, 'Should return 200')
+    t.ok(res.body.projectId, 'Should return projectId')
+    t.ok(res.body.projectName, 'Should return projectName')
+    t.ok(res.body.year, 'Should return year')
+    t.end()
+  })
+})
+
+test.onFinish(() => {
+  if (db.close) db.close()
+  process.exit(0)
 })
